a005a0e991f8a7c2fa52ee98eb55f363f423eac9
Ethan Megill <megill1352@gmail.comm>
2017-05-11 10:33:59 -0400

    ruby problem

---
+++ MorningProblem16/first.rb
@@ -0,0 +1,11 @@
+# Write a method that takes an array of numbers and creates a new array with only the even numbers from the first array.
+
+def even_numbers(array)
+	newarray=[]
+	array.each do |variable|
+		newarray.push(variable) if variable % 2 == 0
+	end
+	newarray
+end
+my_array = [1,2,3,4,5,6,7,8,9,10,13,18,22,23]
+p even_numbers(my_array)
